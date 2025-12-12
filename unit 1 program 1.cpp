241cd75518afafbedb6a1d66de45775428528dae
chetan-034 <kondapallichetu56@gmail.com>
2025-12-12 14:58:29 +0530

    Add files via upload

---
+++ unit 1 program 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+inline int add(int a,int b){
+return a+b;
+}
+inline double add(double a,double b){
+	return a+b;
+}
+inline int add(int a,int b,int c){
+	return a+b+c;
+}
+int main(){
+	cout<<"add(5,10)="<<add(5,10)<< endl;
+	cout<<"add(2.5,3.7)="<<add(2.5,3.7)<< endl;
+	cout<<"add(1,2,3)="<<add(1,2,3)<<endl;
+	cout<<"add(5.2,7.3)="<<add(5.2,7.3)<< endl;
+	cout<<"add(2.5,3.7,7.3)="<<add(2.5,3.7,7.3)<< endl;
+	return 0;
+}
